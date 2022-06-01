5a2e803331123df9442b8ab97a5eb82bf927993c
MateusSilvaTI <mateusssilva1603@gmail.com>
2022-06-01 20:28:44 -0300

    Primeiro Commit

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page 3'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final foto = const CircleAvatar(
+    backgroundImage: NetworkImage(
+        'https://scontent.fumu1-1.fna.fbcdn.net/v/t1.6435-9/73341389_1108627846007786_2096400410787971072_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHjlOrZ1Qa1gu97IWUYLfpOwJGU0GRfsdLAkZTQZF-x0vRbiXXgDw4bztmt_OzV4eIp1_i40jN46-qVAyonWM9A&_nc_ohc=t8n60yaK8f8AX9Joidu&_nc_ht=scontent.fumu1-1.fna&oh=00_AT9qrQB7wK-ssYilHG1XaYyWj0owaMMmYw30QyrNEGJ02A&oe=62BE355E'),
+    radius: 150,
+  );
+  final nome = const Text(
+    'Mateus Silva dos Santos',
+    style: TextStyle(fontSize: 30),
+    textAlign: TextAlign.center,
+  );
+  final botaoTelefone = IconButton(
+    color: Colors.blue,
+    icon: const Icon(Icons.phone),
+    onPressed: () {},
+  );
+
+  final botaoEmail = IconButton(
+    color: Colors.blue,
+    icon: const Icon(Icons.mail),
+    onPressed: () {},
+  );
+
+  final botaoSMS = IconButton(
+    color: Colors.blue,
+    icon: const Icon(Icons.sms),
+    onPressed: () {},
+  );
+
+  Widget build(BuildContext contexto) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          foto,
+          nome,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [botaoTelefone, botaoEmail, botaoSMS],
+          ),
+        ],
+      ),
+    );
+  }
+}
